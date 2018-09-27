6d117c3e50b10de72b73d8c218885448bf056253
John Dill <jdilltech@gmail.com>
2018-09-26 20:52:34 -0600

    Added home View

--- igclone/AppDelegate.swift
+++ igclone/AppDelegate.swift
@@ -16,8 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Build Home UI in Lieu of Storyboard
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = UIViewController()
+        homeViewController.view.backgroundColor = UIColor.blue
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
