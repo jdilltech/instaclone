ab744db5ac02144c25c35b9f4e937656c3cd0fae
John Dill <jdilltech@gmail.com>
2018-10-07 20:41:55 -0600

    Added ImagePost Class and fixed display

---
+++ igclone/ImagePost.swift
@@ -0,0 +1,16 @@
+//
+//  ImagePost.swift
+//  igclone
+//
+//  Created by John Dill on 10/4/18.
+//  Copyright © 2018 John Dill. All rights reserved.
+//
+
+import UIKit
+
+struct ImagePost{
+    var photoName : String
+    var userName : String
+    var numLikes : Int
+    var lastComment : String
+}
--- igclone/PhotoViewCell.swift
+++ igclone/PhotoViewCell.swift
@@ -5,28 +5,26 @@
 //  Created by John Dill on 10/3/18.
 //  Copyright © 2018 John Dill. All rights reserved.
 //
-//TESTING NEW .gitignore FILE
 
 import UIKit
 
 class PhotoViewCell: UITableViewCell {
     
-    let photoUserName: UILabel = {
-        let label = UILabel()
-        label.text = "Dilly Pickles"
+     var photoUserName: UILabel? = {
+        var label = UILabel()
         return label
     }()
     
-    let numLikes: UILabel = {
-        let label = UILabel()
-        label.text = "25 Likes"
+    var numLikes: UILabel? = {
+        var label = UILabel()
         return label
     }()
     
-    let comment: UILabel = {
+    var comment: UILabel? = {
         let label = UILabel()
         let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 17)]
-        let attributedString = NSMutableAttributedString(string: "somecoolcommenter ", attributes:attrs)
+        let commenter = ""
+        let attributedString = NSMutableAttributedString(string: "\(commenter) ", attributes:attrs)
         let normalString = NSMutableAttributedString(string: "This is the single greatest pickle situation I have ever seen.  We can never have enough pickles!  Bring on all the pickles!  Did I mention this comment would be loaded with pickles?  Because, pickles!")
         attributedString.append(normalString)
         label.attributedText = attributedString
@@ -48,23 +46,32 @@ class PhotoViewCell: UITableViewCell {
         return imageView
     }()
     
-    let photo: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "img1"))
+    var photo: UIImageView? = {
+        var imageView = UIImageView(image: UIImage(named: "img1"))
         imageView.clipsToBounds = true
         imageView.backgroundColor = .white
         return imageView
     }()
     
+    var imagePost: ImagePost! {
+        didSet {
+            photo!.image = UIImage(named: imagePost.photoName)
+            photoUserName?.text = imagePost.userName
+            numLikes?.text = "\(imagePost.numLikes)"
+            comment?.text = imagePost.lastComment
+        }
+    }
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        addSubview(photo)
-        addSubview(photoUserName)
+        addSubview(photo!)
+        addSubview(photoUserName!)
         addSubview(likeButton)
         addSubview(commentButton)
-        addSubview(numLikes)
-        addSubview(comment)
+        addSubview(numLikes!)
+        addSubview(comment!)
         
         installConstraints()
     }
@@ -75,32 +82,32 @@ class PhotoViewCell: UITableViewCell {
     }
     
     func installConstraints() {
-        photo.translatesAutoresizingMaskIntoConstraints = false
-        photo.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
-        photo.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
-        photo.topAnchor.constraint(equalTo: topAnchor, constant: 40).isActive = true
-        photo.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -200).isActive = true
+        photo!.translatesAutoresizingMaskIntoConstraints = false
+        photo!.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        photo!.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        photo!.topAnchor.constraint(equalTo: topAnchor, constant: 40).isActive = true
+        photo!.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -200).isActive = true
         
-        photoUserName.translatesAutoresizingMaskIntoConstraints = false
-        photoUserName.leadingAnchor.constraint(equalTo: photo.leadingAnchor, constant: 10).isActive = true
-        photoUserName.bottomAnchor.constraint(equalTo: photo.topAnchor, constant: -10).isActive = true
+        photoUserName!.translatesAutoresizingMaskIntoConstraints = false
+        photoUserName!.leadingAnchor.constraint(equalTo: photo!.leadingAnchor, constant: 10).isActive = true
+        photoUserName!.bottomAnchor.constraint(equalTo: photo!.topAnchor, constant: -10).isActive = true
         
         likeButton.translatesAutoresizingMaskIntoConstraints = false
-        likeButton.leadingAnchor.constraint(equalTo: photo.leadingAnchor, constant: 10).isActive = true
-        likeButton.topAnchor.constraint(equalTo: photo.bottomAnchor, constant: 15).isActive = true
+        likeButton.leadingAnchor.constraint(equalTo: photo!.leadingAnchor, constant: 10).isActive = true
+        likeButton.topAnchor.constraint(equalTo: photo!.bottomAnchor, constant: 15).isActive = true
         
         commentButton.translatesAutoresizingMaskIntoConstraints = false
         commentButton.leadingAnchor.constraint(equalTo: likeButton.trailingAnchor, constant: 15).isActive = true
-        commentButton.topAnchor.constraint(equalTo: photo.bottomAnchor, constant: 15).isActive = true
+        commentButton.topAnchor.constraint(equalTo: photo!.bottomAnchor, constant: 15).isActive = true
         
-        numLikes.translatesAutoresizingMaskIntoConstraints = false
-        numLikes.leadingAnchor.constraint(equalTo: photo.leadingAnchor, constant: 10).isActive = true
-        numLikes.topAnchor.constraint(equalTo: likeButton.bottomAnchor, constant: 15).isActive = true
+        numLikes!.translatesAutoresizingMaskIntoConstraints = false
+        numLikes!.leadingAnchor.constraint(equalTo: photo!.leadingAnchor, constant: 10).isActive = true
+        numLikes!.topAnchor.constraint(equalTo: likeButton.bottomAnchor, constant: 15).isActive = true
         
-        comment.translatesAutoresizingMaskIntoConstraints = false
-        comment.leadingAnchor.constraint(equalTo: photo.leadingAnchor, constant: 10).isActive = true
-        comment.topAnchor.constraint(equalTo: numLikes.bottomAnchor, constant: 10).isActive = true
-        comment.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        comment!.translatesAutoresizingMaskIntoConstraints = false
+        comment!.leadingAnchor.constraint(equalTo: photo!.leadingAnchor, constant: 10).isActive = true
+        comment!.topAnchor.constraint(equalTo: numLikes!.bottomAnchor, constant: 10).isActive = true
+        comment!.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
         
     }
 }
